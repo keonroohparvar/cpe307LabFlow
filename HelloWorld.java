55d3b427001147b5535457b19f9cf1f2b8a4b45a
crossfoot <gross.samuelf@gmail.com>
2021-01-06 11:41:12 -0700

    Don't forget to say Hi to the professor

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
       System.out.println("Hello World, this is my third year at Cal Poly");
+      System.out.println("Hello Prof K.")
     }
   }
